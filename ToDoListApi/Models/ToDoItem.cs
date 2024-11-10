e0faff696850c886bbcbbd925031a04906bc278b
Comfort F. Abraham <Comfort F. Abraham@DESKTOP-4GJ9PJE>
2024-11-10 09:43:49 +0100

    start

--- ToDoListApi/Models/ToDoItem.cs
+++ ToDoListApi/Models/ToDoItem.cs
@@ -1,6 +1,20 @@
-﻿namespace ToDoListApi.Models
+﻿using ToDoListApi.Models;
+
+public class ToDoItem
 {
-    public class ToDoItem
-    {
-    }
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime? DueDate { get; set; }
+
+    public bool IsCompleted { get; set; } = false;
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
 }
